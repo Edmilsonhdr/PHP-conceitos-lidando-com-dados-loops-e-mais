7b11776f57dff1dac0888ef70916c0dac50efa19
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2022-09-04 23:03:37 -0300

    avancando em php

---
+++ desafio/tabuada.php
@@ -0,0 +1,7 @@
+<?php
+
+$multiplicador= 9;
+for($i=1; $i <= 10; $i++){
+    $resultado = $multiplicador * $i;
+    echo "$multiplicador x $i = $resultado" . PHP_EOL;
+}
---
+++ loop-lista.php
@@ -0,0 +1,12 @@
+<?php
+
+$idadeList = [20,10,25,50,85,90,35,28,19,4];
+
+// [
+//     PHP cout()
+//     JAVASCRIPT .lengths
+// ]
+
+for ($i = 0; $i < count($idadeList); $i++) {
+    echo $idadeList[$i] . PHP_EOL;
+}
